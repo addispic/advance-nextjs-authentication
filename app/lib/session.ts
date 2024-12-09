78addb125cd99e2e70735c91bfac8ccebb376054
addispic <addispik@gmail.com>
2024-12-09 17:30:30 +0300

    middleware is added

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -1,4 +1,5 @@
 import {SignJWT, jwtVerify, JWTPayload} from 'jose'
+import { NextRequest, NextResponse } from 'next/server';
 
 // secrete key
 const secretKey = process.env.SECRET_KEY || "";
@@ -12,4 +13,36 @@ export async function encrypt(payload: JWTPayload){
         .setIssuedAt()
         .setExpirationTime("60s from now")
         .sign(encodedKey)
+}
+
+// decrypt
+export async function decrypt(session: string){
+    try{
+        const {payload} = await jwtVerify(session,encodedKey,{algorithms: ['HS256']})
+        return payload
+    }catch(err){
+        console.log(err)
+        console.log("Failed to verify session")
+    }
+}
+
+// update session
+export async function updateSession(request: NextRequest){
+    const session = request.cookies.get("session")?.value;
+    if (!session) return; 
+    const parsed = await decrypt(session)
+    const response = NextResponse.next()
+
+    response.cookies.set({
+        name: 'session',
+        value: await encrypt({_id: parsed?._id}),
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: true,
+        path: "/",
+        expires: new Date(Date.now() + 60 * 1000)
+    })
+
+    return response
+
 }
---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// lib
+import { updateSession } from "./app/lib/session";
+
+export async function middleware(request: NextRequest) {
+  if (!request.cookies.get("session")?.value) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  } else {
+    return await updateSession(request);
+  }
+}
+
+// config
+export const config = {
+  matcher: ["/"],
+};
