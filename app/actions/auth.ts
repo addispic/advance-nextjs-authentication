033569123f83d1c0f757dbcd3951aeac5f1fa0eb
addispic <addispik@gmail.com>
2024-12-09 16:53:36 +0300

    singup is done a bit

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,17 +1,34 @@
-"use server"
-
+"use server";
+import bcrypt from "bcryptjs";
 // db
 import { dbConnectionHandler } from "../db/db-connection";
-
+// models
+// user model
+import UserModel from "../models/user-model";
 // signup
-export async function signup(userCredentials: {username: string; email: string; password: string}){
-    const {username,email,password} = userCredentials 
-    try{
-        await dbConnectionHandler()
-        console.log({username,email,password})
-        return {success: true}
-    }catch(err){
-        console.log(err)
-        return {success: false}
+export async function signup(userCredentials: {
+  username: string;
+  email: string;
+  password: string;
+}) {
+  const { username, email, password } = userCredentials;
+  try {
+    await dbConnectionHandler();
+    const isUsernameExist = await UserModel.findOne({ username });
+    if (isUsernameExist) {
+      return { usernameError: "Username already exist" };
     }
+    const isEmailExist = await UserModel.findOne({ email });
+    if (isEmailExist) {
+      return { emailError: "Email address already exist" };
+    }
+    const newUser = await UserModel.create({
+      username,
+      email,
+      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
+    });
+    return { successMessage: "successful signup" };
+  } catch (err) {
+    return { error: "fail to signup" };
+  }
 }
---
+++ app/models/user-model.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+// user schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true,
+})
+
+// exports
+export default mongoose.models.Users || mongoose.model("Users",userSchema)
