da93ed66c44e6ed87caefccd72092b5cd551b61b
addispic <addispik@gmail.com>
2024-12-09 15:48:49 +0300

    login form UI is done a bit

--- app/(authentication)/login/page.tsx
+++ app/(authentication)/login/page.tsx
@@ -1,5 +1,10 @@
+// ui
+// authentication
+import LoginForm from "@/app/ui/authentication/LoginForm";
 export default function Login(){
     return (
-        <div>Login Page</div>
-    )
+      <div className="w-screen h-screen overflow-hidden flex items-center justify-center">
+        <LoginForm />
+      </div>
+    );
 }
