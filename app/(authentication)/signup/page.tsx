bcc68f7fdace8734efd779b9cbf7e9562603fa3a
addispic <addispik@gmail.com>
2024-12-09 15:43:34 +0300

    signup form UI is done a bit

---
+++ app/(authentication)/signup/page.tsx
@@ -0,0 +1,10 @@
+// ui
+// authentication
+import SignupForm from "@/app/ui/authentication/SignupForm"
+export default function Signup(){
+    return (
+        <div className="w-screen h-screen overflow-hidden flex items-center justify-center">
+            <SignupForm />
+        </div>
+    )
+}
