206f7841faa46d34a6b41a4f007aa14e1a68251a
addispic <addispik@gmail.com>
2024-12-10 13:33:01 +0300

    changes

--- app/ui/dashboard/DashboardHeader.tsx
+++ app/ui/dashboard/DashboardHeader.tsx
@@ -1,13 +1,17 @@
 // icons
 import { GoTasklist } from "react-icons/go";
 import { CiSearch } from "react-icons/ci";
+import { IoIosNotifications } from "react-icons/io";
+import { RiUser6Line } from "react-icons/ri";
+import { IoExitOutline } from "react-icons/io5";
+
 export default function DashboardHeader(){
     return (
         <div className="flex items-center justify-between px-5 py-3">
             {/* left */}
             <div className="flex items-center gap-x-5">
                 {/* toggler */}
-                <div>
+                <div className="hidden">
                     <GoTasklist className="text-3xl text-neutral-500 cursor-pointer"/>
                 </div>
                 {/* search */}
@@ -17,7 +21,21 @@ export default function DashboardHeader(){
                 </div>
             </div>
             {/* right */}
-            <div>right</div>
+            <div className="flex items-center justify-end gap-x-3.5">
+                {/* notification */}
+                <div className="w-[24px] aspect-square rounded-full overflow-hidden border border-green-300 flex items-center justify-center text-green-500 cursor-pointer">
+                    <IoIosNotifications />
+                </div>
+                {/* user profile */}
+                <div className="flex items-center justify-end gap-x-1 cursor-pointer text-sm text-green-500">
+                    <span>Haddis</span>
+                    <RiUser6Line className="text-xl"/>
+                </div>
+                {/* logout */}
+                <button className="text-xl text-green-500">
+                    <IoExitOutline />
+                </button>
+            </div>
         </div>
     )
 }
