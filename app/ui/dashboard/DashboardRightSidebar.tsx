0807e1278722850140660bbd3dd954edf614ea05
addispic <addispik@gmail.com>
2024-12-12 17:14:05 +0300

    image component is done a bit

--- app/ui/dashboard/DashboardRightSidebar.tsx
+++ app/ui/dashboard/DashboardRightSidebar.tsx
@@ -1,10 +1,18 @@
+"use client"
+import Image from 'next/image'
+import myProfileImage from '@/public/pexels-photo-4879302.webp'
+
 export default function DashboardRightSideBar() {
+
+  
   return (
     <div className="min-w-72 h-full px-5 py-1.5 bg-neutral-50">
       {/* profiles */}
       <div className="bg-white shadow-sm p-5 rounded-md overflow-hidden">
         {/* image */}
-        <div>image</div>
+        <div className="relative w-[120px] aspect-square rounded-full overflow-hidden">
+          <Image src={myProfileImage} fill={true} alt='' objectFit='cover'/>
+        </div>
         {/* navigator */}
         <div>navigator</div>
         {/* user info */}
