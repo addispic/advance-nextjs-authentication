a1ed5ff90973ad54213a7d93711cd3856fe36a1a
addispic <addispik@gmail.com>
2024-12-13 11:03:27 +0300

    prifile picker is done a bit

--- app/ui/dashboard/DashboardRightSidebar.tsx
+++ app/ui/dashboard/DashboardRightSidebar.tsx
@@ -1,6 +1,9 @@
-"use client"
+
 import Image from 'next/image'
 import myProfileImage from '@/public/pexels-photo-4879302.webp'
+
+// ui
+import ProfilePicker from './ProfilePicker';
 
 export default function DashboardRightSideBar() {
 
@@ -9,9 +12,15 @@ export default function DashboardRightSideBar() {
     <div className="min-w-72 h-full px-5 py-1.5 bg-neutral-50">
       {/* profiles */}
       <div className="bg-white shadow-sm p-5 rounded-md overflow-hidden">
-        {/* image */}
+        {/* profiles */}
+        <div className='flex items-center justify-center'>
+
         <div className="relative w-[120px] aspect-square rounded-full overflow-hidden">
           <Image src={myProfileImage} fill={true} alt='' objectFit='cover'/>
+
+          {/* add new profile picker */}
+          <ProfilePicker />
+        </div>
         </div>
         {/* navigator */}
         <div>navigator</div>
---
+++ app/ui/dashboard/ProfilePicker.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React from "react";
+// icons
+import { IoIosCamera } from "react-icons/io";
+export default function ProfilePicker() {
+
+    // profile submit handler
+    const profileSubmitHandler = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        if(event.target.files?.[0]){
+            const formData = new FormData()
+            formData.append("file", event.target.files?.[0]);
+            formData.append("upload_preset", "menelik");
+
+            const data = await fetch(
+              "https://api.cloudinary.com/v1_1/diyn4opd7/image/upload",
+              {
+                method: "POST",
+                body: formData,
+              }
+            ).then((r) => r.json());
+            console.log(data);
+        }
+        
+    }
+  return (
+    <div>
+      <input
+        type="file"
+        name="profile"
+        id="profile-picker"
+        accept="image/*"
+        hidden 
+        onChange={profileSubmitHandler}
+      />
+      <label
+        htmlFor="profile-picker"
+        className="absolute left-1/2 -translate-x-1/2 bottom-0 cursor-pointer"
+      >
+        <div className="w-[24px] aspect-square rounded-md overflow-hidden flex items-center justify-center bg-white text-green-500 text-lg">
+          <IoIosCamera />
+        </div>
+      </label>
+    </div>
+  );
+}
