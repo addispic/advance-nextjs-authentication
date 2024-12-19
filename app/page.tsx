df561a7f7fb1ed319cd521a67d2f0e827c500045
addispic <addispik@gmail.com>
2024-12-19 20:39:42 +0300

    tanstack project introduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+
+// interface
+interface User {
+  id: number;
+  email: string;
+  name: string;
+}
+
 export default function Home() {
-  return <div>Home</div>;
+  const { data } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+        res.json()
+      ),
+    select: (users) => users.map((user) => ({ id: user.id, name: user.name })),
+  });
+  console.log(data);
+  return <div>Haddis Menelik From Bahir Dar</div>;
 }
